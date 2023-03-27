8d2b8016647c28681dd4fc6468655719e9352abf
Sérgio Branco <7887134+asergiobranco@users.noreply.github.com>
2023-03-27 22:47:07 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Tiny Embedded Intelligence Layer'
+copyright = '2023, Branco'
+author = 'Sergio Branco'
 
 release = '0.1'
 version = '0.1.0'
